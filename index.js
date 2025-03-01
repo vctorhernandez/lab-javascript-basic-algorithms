6260863ae9435501e5232e39f1780ee23211f79a
Víctor Hernandez <hernandezgarridovictor@gmail.com>
2025-03-01 19:59:35 +0100

    Solved lab

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Iteration 1: Names and Input
-let hacker1 = "abc"
+let hacker1 = "Víctor"
 console.log("The driver's name is", hacker1);
-let hacker2 = "abc"
+let hacker2 = "Marcos"
 console.log("The navigator's name is", hacker2);
 
 // Iteration 2: Conditionals
